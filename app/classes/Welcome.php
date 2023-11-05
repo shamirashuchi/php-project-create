302ca11d6f97edf8bc1dbef6f6399515fd0a3329
shamirashuchi <shamirashuchi@gmail.com>
2023-11-05 20:55:15 +0600

    2nd

--- app/classes/Welcome.php
+++ app/classes/Welcome.php
@@ -4,10 +4,18 @@
 namespace App\classes;
 //composer resource khujbe namespace diye
 //namespace is a syntax of oop  which is used for directory
-
+// variable is memory location address
+//variable major rules
+//start with $
+//a-z,A-Z,0-9
+//no number in first
+//standard rules
+//small letter
+//meaning full
+//readable
 class Welcome
 {
-    public $message; //property declare
+    public $message, $firstName,$lastName; //property declare
     //nijossho method
     public function  __construct()
     {
@@ -17,6 +25,16 @@ class Welcome
     //created method of our own
     public function index()
     {
-        echo $this->message;// property print
+        //echo $this->message;// property print
+        $this->firstName = "BASIS";
+        $this->firstName = true;
+        $this->lastName = 123;
+        $this->lastName = 123.25;
+        echo gettype($this->firstName);
+        echo gettype($this->lastName);
+//        echo $this->firstName . " " . $this->lastName;
+//        echo $this->firstName . "&nbsp" . $this->lastName;
+//        echo $this->firstName . "&nbsp &nbsp" . $this->lastName;
+
     }
 }
