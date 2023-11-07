13ba572e4523a019d8568ca819bfce2358e7152a
shamirashuchi <shamirashuchi@gmail.com>
2023-11-07 20:09:56 +0600

    operator

--- app/classes/Welcome.php
+++ app/classes/Welcome.php
@@ -15,7 +15,7 @@ namespace App\classes;
 //readable
 class Welcome
 {
-    public $message, $firstName,$lastName; //property declare
+    public $message, $firstName,$lastName,$firstNumber,$secondNumber,$result; //property declare
     //nijossho method
     public function  __construct()
     {
@@ -26,15 +26,183 @@ class Welcome
     public function index()
     {
         //echo $this->message;// property print
-        $this->firstName = "BASIS";
-        $this->firstName = true;
-        $this->lastName = 123;
-        $this->lastName = 123.25;
-        echo gettype($this->firstName);
-        echo gettype($this->lastName);
+//        $this->firstName = "BASIS";
+//        $this->firstName = true;
+//        $this->lastName = 123;
+//        $this->lastName = 123.25;
+//        echo gettype($this->firstName);
+//        echo gettype($this->lastName);
+        //&nbsp for space
 //        echo $this->firstName . " " . $this->lastName;
 //        echo $this->firstName . "&nbsp" . $this->lastName;
 //        echo $this->firstName . "&nbsp &nbsp" . $this->lastName;
 
+
+        //Operator
+        //a syntax for doing many task
+        $this->firstNumber =10;
+        $this->secondNumber =20;
+        $this->result      = 30;
+
+
+
+        //nor operator
+//        $this->r = true;
+//        $this->y = false;
+//        echo $this->r ;//1
+//        echo '<br/>============================<br/>';
+//        echo !$this->r ;//blank
+//        echo '<br/>============================<br/>';
+//        echo $this->y ;//blank
+//        echo '<br/>============================<br/>';
+//        echo !$this->y ;//1
+//        echo '<br/>============================<br/>';
+
+
+
+
+        //logical operator
+//        echo '<br/>=========================<br/>';
+//        echo $this->firstNumber < $this->secondNumber  && $this->firstNumber < $this->result;//1
+//
+//        echo '<br/>=========================<br/>';
+//        echo $this->firstNumber > $this->secondNumber  && $this->firstNumber > $this->result;//blank
+//
+//        echo '<br/>=========================<br/>';
+//        echo $this->firstNumber < $this->secondNumber  && $this->firstNumber > $this->result;//blank
+//
+//        echo '<br/>=========================<br/>';
+//        echo $this->firstNumber > $this->secondNumber  && $this->firstNumber < $this->result;//blank
+
+
+
+
+//        echo '<br/>=========================<br/>';
+//        echo $this->firstNumber < $this->secondNumber  || $this->firstNumber < $this->result;//1
+//
+//        echo '<br/>=========================<br/>';
+//        echo $this->firstNumber > $this->secondNumber  || $this->firstNumber > $this->result;//blank
+//
+//        echo '<br/>=========================<br/>';
+//        echo $this->firstNumber < $this->secondNumber  || $this->firstNumber > $this->result;//1
+//
+//        echo '<br/>=========================<br/>';
+//        echo $this->firstNumber > $this->secondNumber  || $this->firstNumber < $this->result;//1
+
+
+
+
+
+
+
+
+        //conditional operator
+        //true hole 1
+        //false hole kisu ase na ba blank
+//        echo '<br/>';
+//        echo $this->firstNumber > $this->secondNumber;
+//
+//
+//        echo '<br/>';
+//        echo $this->firstNumber >= $this->secondNumber;
+//
+//
+//        echo '<br/>';
+//        echo $this->firstNumber < $this->secondNumber;
+//
+//
+//        echo '<br/>';
+//        echo $this->firstNumber <= $this->secondNumber;
+//
+//        echo '<br/>';
+//        echo $this->firstNumber == $this->secondNumber;
+//
+//        echo '<br/>';
+//        echo $this->firstNumber != $this->secondNumber;
+//
+//        echo '<br/>';
+//        echo $this->firstNumber === $this->secondNumber;
+//
+//        echo '<br/>';
+//        echo $this->firstNumber !== $this->secondNumber;
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Assignable operator
+//        echo '<br/>';
+//        echo $this->firstNumber += $this->secondNumber;//30//$this->firstNumber = $this->firstNumber + $this->secondNumber
+//
+//        echo '<br/>';
+//        echo $this->firstNumber -= $this->secondNumber;//10//$this->firstNumber = $this->firstNumber - $this->secondNumber
+//
+//        echo '<br/>';
+//        echo $this->firstNumber *= $this->secondNumber;//200
+//
+//        echo '<br/>';
+//        echo $this->firstNumber /= $this->secondNumber;//10
+//
+//        echo '<br/>';
+//        echo $this->firstNumber %= $this->secondNumber;//10
+//
+//        echo '<br/>';
+//        echo $this->firstNumber .= $this->secondNumber;//1020  // . mane concate
+
+
+
+
+
+
+//arithmatic operator
+//        echo - $this->firstNumber;//negative to positive and positive to negative
+//        echo '<br/>';
+//        echo $this->firstNumber + $this->secondNumber;
+//
+//
+//        echo '<br/>';
+//        echo $this->firstNumber - $this->secondNumber;
+//
+//        echo '<br/>';
+//        echo $this->firstNumber * $this->secondNumber;
+//
+//        echo '<br/>';
+//        echo $this->firstNumber / $this->secondNumber;
+//
+//        echo '<br/>';
+//        echo $this->firstNumber % $this->secondNumber;
+
+
+        //php do  interprate
+        //echo part,$this->firstNumber part ,++ part
+        //part part execute kore tai   echo $this->firstNumber++; give 20 then updated to 21
+//        echo '<br/>';
+//        echo $this->firstNumber++;//post increment 20
+//        echo '<br/>';
+//        echo $this->firstNumber;//21
+//        echo '<br/>';
+//        echo ++$this->firstNumber;//pre increment 22
+//        echo '<br/>';
+//        echo $this->firstNumber;
+//
+//
+//        echo '<br/>';
+//        echo --$this->firstNumber;//pre deccrement 21
+//        echo '<br/>';
+//        echo $this->firstNumber--;//21
+//        echo '<br/>';
+//        echo $this->firstNumber--;//post decrement 20
+//        echo '<br/>';
+//        echo --$this->firstNumber;//18
     }
+
+
 }
